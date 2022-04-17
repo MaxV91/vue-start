41de5d476f03f13579df06ad8509b006b557c76b
Maxim K <max.kotsurenko@gmail.com>
2022-04-17 17:10:56 +0300

    events done

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert('submitted');
+    },
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
     }
   }
 });
